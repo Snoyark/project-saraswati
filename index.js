32f4d93a219812eb8879b5f8d766aa863fb547f6
Neelmira Venugop <neelv7@berkeley.edu>
2024-03-17 19:21:06 -0700

    add basic configs to start project

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.main = void 0;
+function main() {
+    return 'this is the main function?';
+}
+exports.main = main;
---
+++ index.ts
@@ -0,0 +1,5 @@
+
+
+export function main(): string {
+  return 'this is the main function?'
+}
