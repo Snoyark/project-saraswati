0e9ad00cf696f74f72dbae83bd205111b7baa4e5
snoyark <neelv7@berkeley.edu>
2024-03-18 07:40:01 -0700

    add in ollama and create test main

--- index.ts
+++ index.ts
@@ -1,8 +1,13 @@
 
+import ollama from 'ollama'
 
-export function main(): string {
-  console.log('this is the main')
-  return 'this is the main function?'
+export async function main(): Promise<number> {
+  const response = await ollama.chat({
+    model: 'llama2',
+    messages: [{ role: 'user', content: 'Why is the sky blue?' }],
+  })
+  console.log(response.message.content)
+  return 0
 }
 
 main()
